01bbe2d35e407e1d2fe33dde32b7a609411bd422
marjika <bluegoo5@hotmail.com>
2018-07-23 20:16:31 -0600

    functioning

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger Classic', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burn-me-up Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hearty Double', false);
